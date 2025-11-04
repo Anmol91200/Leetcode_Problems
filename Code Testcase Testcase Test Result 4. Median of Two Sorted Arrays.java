6d476690c380901e59a3457f695353ca18437021
Anmol Rathore <rathoreanmol1350@gmail.com>
2025-11-05 00:26:22 +0530

    Create  Code Testcase Testcase Test Result 4. Median of Two Sorted Arrays.java

---
+++ Code Testcase Testcase Test Result 4. Median of Two Sorted Arrays.java
@@ -0,0 +1,27 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] arr = new int[m + n];
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                arr[k++] = nums1[i++];
+            } else {
+                arr[k++] = nums2[j++];
+            }
+        }
+        while (i < m)
+            arr[k++] = nums1[i++];
+        while (j < n)
+            arr[k++] = nums2[j++];
+
+        int mid = (m + n) / 2;
+        if((m+n)==0)
+        return 0.0;
+        if ((m+n)% 2 == 0) {
+            return (arr[mid - 1] + arr[mid]) / 2.0;
+        } else
+            return arr[mid];
+    }
+}
