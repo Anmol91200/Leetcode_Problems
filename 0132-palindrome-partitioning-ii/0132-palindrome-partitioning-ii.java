c4612ed27fd9538f7329be6fdd721ce709435ffc
Anmol Rathaur <rathoreanmol1350@gmail.com>
2026-07-19 23:30:39 +0530

    Time: 11 ms (98.55%), Space: 42.4 MB (96.79%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -3,36 +3,29 @@ class Solution {
         int n = s.length();
         if (n <= 1) return 0;
 
-        boolean[][] isPal = new boolean[n][n];
-        for (int i = 0; i < n; i++) {
-            isPal[i][i] = true;
-        }
-        for (int length = 2; length <= n; length++) {
-            for (int i = 0; i <= n - length; i++) {
-                int j = i + length - 1;
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (length == 2 || isPal[i + 1][j - 1]) {
-                        isPal[i][j] = true;
-                    }
-                }
-            }
-        }
-
         int[] dp = new int[n];
         for (int i = 0; i < n; i++) {
-            if (isPal[0][i]) {
-                dp[i] = 0;
-            } else {
-                int minCuts = i; 
-                for (int j = 0; j < i; j++) {
-                    if (isPal[j + 1][i]) {
-                        minCuts = Math.min(minCuts, dp[j] + 1);
-                    }
-                }
-                dp[i] = minCuts;
-            }
+            dp[i] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            expand(s, i, i, dp);
+            expand(s, i, i + 1, dp);
         }
 
         return dp[n - 1];
+    }
+
+    private void expand(String s, int left, int right, int[] dp) {
+        int n = s.length();
+        while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+            if (left == 0) {
+                dp[right] = 0;
+            } else {
+                dp[right] = Math.min(dp[right], dp[left - 1] + 1);
+            }
+            left--;
+            right++;
+        }
     }
 }
