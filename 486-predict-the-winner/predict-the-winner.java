3cf346c7fa742d949195d8459ad9c213a91e9c17
Anmol Rathaur <rathoreanmol1350@gmail.com>
2026-08-01 18:26:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (74.96%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] memo = new int[n][n];
+        return maxScoreDiff(nums, 0, n - 1, memo) >= 0;
+    }
+
+    private int maxScoreDiff(int[] nums, int i, int j, int[][] memo) {
+        if (i == j) {
+            return nums[i];
+        }
+        if (memo[i][j] != 0) {
+            return memo[i][j];
+        }
+
+        int pickLeft = nums[i] - maxScoreDiff(nums, i + 1, j, memo);
+        int pickRight = nums[j] - maxScoreDiff(nums, i, j - 1, memo);
+
+        memo[i][j] = Math.max(pickLeft, pickRight);
+        return memo[i][j];
+    }
+}
