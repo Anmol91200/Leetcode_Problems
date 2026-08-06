7b1aa276cc4a5c5ffb6e7aa30a468a9bd9ec08da
Anmol Rathaur <rathoreanmol1350@gmail.com>
2026-08-06 22:52:00 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (50.26%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+
+    for(int i = n ; ;i++){
+        int pdct=1;
+        int num=i;
+        while(num>0){
+            pdct *= num%10;
+            num /=10;
+        }
+        if(pdct%t==0){
+        return i;
+        }
+    }
+        
+    }
+}
